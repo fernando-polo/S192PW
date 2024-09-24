d3eed1e017f665baeec28246eb8042e97c448032
fernando-polo <fernandogmzmdo@gmail.com>
2024-09-24 17:54:02 -0600

    add IntroLaravel

---
+++ IntroLaravel/resources/js/app.js
@@ -0,0 +1,11 @@
+import './bootstrap';
+
+// Agregar siguientes comandos:
+import '../scss/app.scss';
+import * as bootstrap from 'bootstrap';
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
