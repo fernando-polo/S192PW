0a362f8e28d5af76bcfce6de565af82b3ada5830
fernando-polo <fernandogmzmdo@gmail.com>
2024-10-09 18:59:29 -0600

    Creación del controlador.
    Se creó el controlador y se modificó la sintaxis para llamar a las vistas en el archivo web.php

--- IntroLaravel/resources/views/components/alert.blade.php
+++ IntroLaravel/resources/views/components/alert.blade.php
@@ -1,5 +1,7 @@
-<div class="container mt-4"></div>
+<div class="container mt-4">
 
-<div class="alert alert-{{$tipo}}" role="alert">
+  <div class="alert alert-{{$tipo}}" role="alert">
     Soy un Alert! {{ $slot }}
   </div>
+
+</div>
--- IntroLaravel/routes/web.php
+++ IntroLaravel/routes/web.php
@@ -1,19 +1,36 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\controladorVistas;
 use Illuminate\Support\Facades\Route;
 
 
+// Nueva sintáxis de la ruta, definimos la ruta, mandamos a llamar al controlador y su función y el apodo de las vistas
+Route::get('/', [controladorVistas::class, 'home'])->name('rutainicio');
+Route::get('/formulario', [controladorVistas::class, 'formulario'])->name('rutaform');
+Route::get('/clientes', [controladorVistas::class, 'consulta'])->name('rutaclientes');
+Route::view('/component', 'componentes')->name('rutacomponentes');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+
+
+
+
+
+
+
+
+
+
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
 
 // Ruta de tipo GET
 // Route::get('/', function () {
@@ -21,14 +38,13 @@ Route::middleware('auth')->group(function () {
 // });
 
 // Ruta de tipo vista
-Route::view('/', 'inicio')->name('rutainicio');
+// Route::view('/', 'inicio')->name('rutainicio');
 
-Route::view('/formulario', 'formulario')->name('rutaform');
+// Route::view('/formulario', 'formulario')->name('rutaform');
 
-Route::view('/clientes', 'clientes')->name('rutaclientes');
+// Route::view('/clientes', 'clientes')->name('rutaclientes');
 
-Route::view('/component', 'componentes')->name('rutacomponentes');
+// Route::view('/component', 'componentes')->name('rutacomponentes');
 
 
-
-require __DIR__.'/auth.php';
+// require __DIR__.'/auth.php';
