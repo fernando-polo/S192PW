eba7924c66d1317331e9643417491e98bce86c2e
fernando-polo <fernandogmzmdo@gmail.com>
2024-10-16 11:00:43 -0600

    Portada
    Se creó la portada con sus debidos componentes.

--- practicaRepasos/resources/views/welcome.blade.php
+++ practicaRepasos/resources/views/welcome.blade.php
@@ -8,6 +8,38 @@
     <title>practicaRepasos</title>
 </head>
 <body>
-    <h1>Hola mundo</h1>
-</body>
+    <div class="container">
+
+            <h3 class="text-end mt-5">El Marqués, Querétaro a miércoles 16 de octubre de 2024</h3>
+
+            <h1 class="text-center mt-5">Universidad Politécnica de Querétaro</h1>
+            <h2 class="text-center">Ingeniería en Sistemas Computacionales </h2>
+
+            <div class="text-center">
+            <img src="{{ asset('images/LogoUPQ.png') }}" alt="Logo_UPQ" class="mt-3">
+            </div>
+
+            <h1 class="text-center mt-5">Programación web</h1>
+            <h2 class="text-center">Práctica: prácticaRepasos</h2>
+            <h3 class="text-center">Parcial 2</h3>
+
+            <div class="text-center mt-4">
+            <button type="button" class="btn btn-danger">Repaso - 1</button>
+            </div>
+
+            <br>
+            <br>    
+
+            <div>
+            <h3><strong>Alumno:</strong>Fernando Gómez Maldonado</h3>
+            <h3><strong>Docente:</strong>Ivan Isay Guerra López</h3>
+            <h3><strong>Grupo:</strong> S192-7</h3>
+            </div>
+
+
+
+
+    </div>
+
+</body> 
 </html>
